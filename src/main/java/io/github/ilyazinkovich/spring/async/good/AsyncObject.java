dff992403bf9b682d41b608b9c4ace74f7bd6fb4
IlyaZinkovich <ilya.zinkovich@gmail.com>
2018-10-19 10:30:07 +0200

    Added CompletableFuture examples without tests

---
+++ src/main/java/io/github/ilyazinkovich/spring/async/good/AsyncObject.java
@@ -0,0 +1,49 @@
+package io.github.ilyazinkovich.spring.async.good;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class AsyncObject {
+
+  private final Executor executor;
+
+  public AsyncObject(final Executor executor) {
+    this.executor = executor;
+  }
+
+  public void asyncMethod() {
+    CompletableFuture.runAsync(() -> System.out.println("execute asynchronously"), executor);
+  }
+
+  public CompletableFuture<Void> explicitlyAsyncMethod() {
+    return CompletableFuture.runAsync(() -> System.out.println("execute asynchronously"), executor);
+  }
+
+  public void regularMethodCallingAsync() {
+    asyncMethod();
+  }
+
+  public void regularMethodCallingExplicitlyAsync() {
+    explicitlyAsyncMethod().thenRun(() -> System.out.println("callback"));
+  }
+
+  public CompletableFuture<Integer> asyncMethodWithRawReturnType() {
+    return CompletableFuture.runAsync(() -> System.out.println("execute asynchronously"), executor)
+        .thenApplyAsync(noResult -> 1);
+  }
+
+  public CompletableFuture<Integer> asyncMethodThrowingException() {
+    return CompletableFuture.runAsync(() -> System.out.println("execute asynchronously"))
+        .thenApply(noResult -> {
+          if (new Random().nextBoolean()) {
+            throw new RuntimeException("async exception");
+          } else {
+            return 1;
+          }
+        }).exceptionally(throwable -> {
+          throwable.printStackTrace();
+          return 1;
+        });
+  }
+}
